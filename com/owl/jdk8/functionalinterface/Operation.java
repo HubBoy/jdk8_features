4e2275800e48497ba50c21e2dcef8393516a8b11
ZeroTo <owl_zeroto@163.com>
2017-08-15 10:15:31 +0800

    init commit

---
+++ com/owl/jdk8/functionalinterface/Operation.java
@@ -0,0 +1,14 @@
+package com.owl.jdk8.functionalinterface;
+
+/**
+ * 接口函数 实现
+ * @author ZeroTo
+ */
+public class Operation implements IOperation{
+
+    @Override
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
